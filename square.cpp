02f0dcb4e773bee149c6d701f29a9b204152a542
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-04-06 21:21:55 +0530

    codeforces 1062

--- square.cpp
+++ square.cpp
@@ -8,7 +8,7 @@ int main(){
     int a, b, c, d;
     cin >> a>> b>>c>>d;
     if (a >= 1 and a <= 10 and b>= 1 and b<= 10 and c>= 1 and c<= 10 and d>= 1 and d<= 10){
-      if (a ==b==c==d){
+      if (a ==b && b==c && c==d){
         cout << "YES" << endl;
       }else{
         cout << "NO" << endl;
