5ce9c17a0db31fb9978c3ee46b7ba7b2e146b7c3
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-04-07 20:09:46 +0530

    codeforces 1062

---
+++ yourname.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int z;
+    cin >> z;
+    for (int i =0; i< z; i++){
+        int a;
+        cin >> a;
+        
+    }
+
+    return 0;
+}
